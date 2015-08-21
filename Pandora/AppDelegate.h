04803a5a17eb030d0f1faa4c519cd97fa0e2e061
Oleg Anghelov <dumoko@gmail.com>
2015-08-22 01:43:18 +0300

    added project

---
+++ Pandora/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pandora
+//
+//  Created by Olegas on 22/08/2015.
+//  Copyright (c) 2015 SkyOffice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pandora/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pandora
+//
+//  Created by Olegas on 22/08/2015.
+//  Copyright (c) 2015 SkyOffice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
