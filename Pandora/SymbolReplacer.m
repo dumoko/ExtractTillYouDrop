da914aac5879c497673fff280a274e7577342546
Oleg Anghelov <dumoko@gmail.com>
2015-08-25 12:26:46 +0300

    exctracted regex, results search

--- Pandora/SymbolReplacer.m
+++ Pandora/SymbolReplacer.m
@@ -12,6 +12,7 @@
 
 @property (nonatomic, copy) NSString *stringToReplace;
 @property (nonatomic, copy) NSMutableArray *alreadyReplaced;
+@property (nonatomic, copy) NSRegularExpression *regex;
 
 @end
 
@@ -22,23 +23,15 @@
     if (self) {
         _stringToReplace = stringToReplace;
         _alreadyReplaced = [NSMutableArray array];
+        NSString *pattern = @"\\$([a-z]{2})";
+        _regex = [NSRegularExpression regularExpressionWithPattern:pattern options:0 error:nil];
     }
 
     return self;
 }
 
 - (NSString *)replace {
-    NSString *pattern = @"\\$([a-z]{2})";
-    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:0 error:nil];
-    NSRange fullStringRange = NSMakeRange(0, self.stringToReplace.length);
-    NSArray *results = [regex matchesInString:self.stringToReplace options:0 range:fullStringRange];
-    NSMutableArray *matches = [NSMutableArray array];
-    for (NSTextCheckingResult *result in results) {
-        NSRange matchRange = [result rangeAtIndex:1];
-        [matches addObject:[self.stringToReplace substringWithRange:matchRange]];
-    }
-
-    for (NSString *match in matches) {
+    for (NSString *match in [self findMatchingResults]) {
         if (![self.alreadyReplaced containsObject:match]) {
             [self.alreadyReplaced addObject:match];
             NSString *replaceTarget = [NSString stringWithFormat:@"$%@", match];
@@ -46,12 +39,25 @@
         }
     }
 
-
     return self.stringToReplace;
 }
 
 
+- (NSArray *)findMatchingResults {
+    NSRange fullStringRange = NSMakeRange(0, self.stringToReplace.length);
+    NSArray *results = [self.regex matchesInString:self.stringToReplace options:0 range:fullStringRange];
+    NSMutableArray *matches = [NSMutableArray array];
+    for (NSTextCheckingResult *result in results) {
+        NSRange matchRange = [result rangeAtIndex:1];
+        [matches addObject:[self.stringToReplace substringWithRange:matchRange]];
+    }
+
+    return matches;
+}
+
+
 - (NSString *)translate:(NSString *)string {
+    
     return [string uppercaseString];
 }
 
